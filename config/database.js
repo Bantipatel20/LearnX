5d4a3383f0abfae00475deed7e41d47bbaf655ff
Bantipatel20 <patelbanti993@gmail.com>
2025-09-27 11:04:30 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,9 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = ()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-        useUnifiedTopology:true,
-        useNewUrlParser:true
+    mongoose.connect(process.env.MONGODB_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
     })
-    .then(()=>console.log())
+    .then(() => console.log("Database Connected Successfully"))
+    .catch((err) => {
+        console.error("Error While Database Connection:", err);
+        process.exit(1);
+    });
 }
+
+export default connectDB;

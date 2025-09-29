8b0d1e9d9cfe1af5102ab9313ab2e4863b6565df
Bantipatel20 <patelbanti993@gmail.com>
2025-09-29 22:22:41 +0530

    Update SubSection.js

--- controllers/SubSection.js
+++ controllers/SubSection.js
@@ -3,6 +3,7 @@ import Section from '../models/Section.js';
 import uploadImageToCloudinary from '../utils/imageUploader.js';
 import dotenv from 'dotenv';
 
+
 dotenv.config();
 
 const createSubSection  = async(req ,res)=>{
@@ -19,11 +20,74 @@ const createSubSection  = async(req ,res)=>{
 
         const videoUrl = await uploadImageToCloudinary(video,process.env.FOLDER_NAME);
 
+        const newSubSection = await SubSection.create({title,timeDuration,description,videoUrl});
+
+        const updateSection = await Section.findByIdAndUpdate({sectionId},{
+            $push:{
+                SubSection:newSubSection._id
+            }
+        },{new:true}).populate("Instructor").exec();
+
+        return res.status(200).json({
+            success:true,
+            message:"SubSection created successfully",
+            updateSection
+        })
 
     }catch(err){
         return res.status(500).json({
             success:false,
-            message:"Error while creating the subsection"
+            message:"Error while creating the subsection",
+            error:err.message
         })
     }
 }
+
+
+const updateSubSection = async(req,res)=>{
+    try{
+        const {updateValue} =req.body;
+
+        const updateNewSubSection = await SubSection.findByIdAndUpdate({subSectionId} ,{updateValue},{new:true});
+
+        return res.status(200).json({
+            success:true,
+            message:"SubSection updating successfully",
+            updateNewSubSection
+        })
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Error while updatating subsection",
+            error:err.message
+        })
+    }
+} 
+
+
+const deleteSubSection = async(req,res)=>{
+    try{
+        const {subSectionId , sectionId} = req.body;
+        await SubSection.findByIdAndDelete(subSectionId);
+
+     await Section.findByIdAndUpdate({_id:sectionId},{
+        $pop:{
+            subSection:subSectionId
+        }
+     },{new:true});
+
+     return res.status(200).json({
+        success:true,
+        message:"SubSection Deleting successfully"
+     })
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Error while deleting the subsection",
+            error:err.message
+        })
+    }
+}
+
+
+export { createSubSection, deleteSubSection, updateSubSection };
